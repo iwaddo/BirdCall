cb59110d2eeca1b02afa71f027046d1fddde60a0
Ian Waddington <iwaddo@icloud.com>
2022-09-24 17:13:00 +0100

    This is to test the commit

--- BirdCall/ViewController.swift
+++ BirdCall/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var youPressedButton: UILabel!
 
+    var audioPlayer: AVAudioPlayer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func birdButtonPressed(_ sender: UIButton) {
+        youPressedButton.text = "You pressed button \(sender.tag)"
+        
+        if let sound = NSDataAsset(name: "bird" + String(sender.tag)) {
+            do {
+               try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: \(error.localizedDescription)Could not initialize AVAudioPlayer Object")
+            }
 
+        } else {
+            print("ERROR: Could not read data from sound file")
+        }
+
+    }
+    
 }
 
